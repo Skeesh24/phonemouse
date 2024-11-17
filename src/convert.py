2ead3e5e9816dc6647e12fadabab6494419f5cb6
pakulov_de <pakulov_de@devtrix.ru>
2024-11-17 16:52:19 +0500

    achieved mouse interaction

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,35 @@
-from random import randint
+import logging
+import tkinter as tk
+
+ANGLE_RANGE = 60  # Длина половины диапазона углов
+
+
+def get_screen_resolution():
+    root = tk.Tk()
+    root.withdraw()  # Скрыть главное окно
+    width = root.winfo_screenwidth()
+    height = root.winfo_screenheight()
+    return width, height
 
 
 def gyroscope_convert(
-    alpha: float,
-    beta: float,
+    _: float,
     gamma: float,
+    beta: float,
+    *,
+    width: float,
+    height: float,
 ) -> tuple[int, int]:
-    return (
-        randint(200, 500),
-        randint(200, 500),
-    )
+    # Получаем разрешение экрана
+
+    # Вычисляем масштаб для перевода углов в пиксели
+    gamma_scale = width / (2 * ANGLE_RANGE)
+    beta_scale = height / (2 * ANGLE_RANGE)
+
+    # Преобразуем углы в экранные координаты
+    x = width / 2 + gamma * gamma_scale
+    y = height / 2 - beta * beta_scale  # Инвертируем, так как ось Y направлена вниз
+
+    logging.info(f"X: {x}, Y: {y}")
+
+    return int(x), int(y)
--- src/executor.py
+++ src/executor.py
@@ -2,20 +2,24 @@ import logging
 
 from mouse import move
 
-from convert import gyroscope_convert
+from convert import get_screen_resolution, gyroscope_convert
 from network import get_state
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 async def main_loop():
+    old_x, old_y = 0, 0
+    x, y = get_screen_resolution()
+    x /= 2
+    y /= 2
     while True:
         phonemeta = await anext(get_state())
 
         if not phonemeta:
             break
 
-        logging.debug(phonemeta)
+        new_x, new_y = gyroscope_convert(*phonemeta, width=x, height=y)
+        move(new_x - old_x, new_y - old_y, absolute=False)
+        old_x, old_y = new_x, new_y
 
-        x, y = gyroscope_convert(*phonemeta)
-        move(x, y, absolute=True)
