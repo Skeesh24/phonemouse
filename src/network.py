1d1509e7ad795054b0efd3e37b1cd64ca09347ee
pakulov_de <pakulov_de@devtrix.ru>
2024-11-17 19:52:44 +0500

    adapt to redis broker

--- src/network.py
+++ src/network.py
@@ -7,11 +7,19 @@ logging.basicConfig(level=logging.DEBUG)
 
 async def get_state():
     async with AsyncClient() as client:
-        async with client.stream("GET", "http://localhost:8000/api/sse/") as response:
+        async with client.stream("GET", "http://localhost:10000/api/sse/") as response:
             async for line in response.aiter_lines():
                 if line.startswith("data:"):
                     data = line[len("data:") :].strip()
                     logging.debug(f"Received data: {data}")
                     # Обработка данных по мере их поступления
-                    yield tuple(map(float, data.split(",")))
+                    data = data.strip("b'")
 
+                    logging.info(data)
+
+                    yield tuple(
+                        float(value)
+                        for value in data.split(",")
+                    )
+                    
+                
